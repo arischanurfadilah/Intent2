1bcf062c656f2fdb05c46af246c6cc219f7ae826
Arischa Nur Fadilah <ariskanf12@gmail.com>
2016-10-06 12:52:30 +0700

    Initial Commit startActivityForResult Final

---
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent2/InputActivity.java
@@ -0,0 +1,57 @@
+package id.sch.smktelkom_mlg.learn.intent2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+public class InputActivity extends AppCompatActivity {
+
+    private static final String NAMA = "Nama";
+    private static final String TAHUN_LAHIR = "TahunLahir";
+    private static final int REQUEST_CODE = 123;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input);
+
+        findViewById(R.id.buttonFinish).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText etNama = (EditText) findViewById(R.id.editTextNama);
+                EditText etThnLhr = (EditText) findViewById(R.id.editTextThnLhr);
+                String nama = etNama.getText().toString();
+                String thnLhrStr = etThnLhr.getText().toString();
+
+                int thnLhr = thnLhrStr.isEmpty()?
+                        Calendar.getInstance().get(Calendar.YEAR):
+                        Integer.parseInt(thnLhrStr);
+
+                Intent intent = new Intent();
+                intent.putExtra(NAMA,nama);
+                intent.putExtra(TAHUN_LAHIR,thnLhr);
+
+                setResult(RESULT_OK,intent);
+                finish();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode== REQUEST_CODE){
+            if (resultCode==RESULT_OK){
+                String nama = data.getStringExtra(InputActivity.NAMA);
+                int thnLhr = data.getIntExtra(InputActivity.TAHUN_LAHIR, 0);
+                TextView tvBiodata = (TextView) findViewById(R.id.textViewBiodata);
+                tvBiodata.setText("Nama\t\t\t\t\t\t\t\t:"+nama+"\nTahun Kelahran\t: "+thnLhr);
+            }
+        }
+    }
+}
